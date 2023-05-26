b45f3e188de661c7a543ac5271a485f52c5c3ff6
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-05-27 02:42:27 +0530

    Add files via upload

---
+++ java/array/nullarray.java
@@ -0,0 +1,14 @@
+
+// in c/c++ if you will get garbage values for null aray 
+// in java you will get all 0 values insted of garbage values
+import java.util.*;
+public class nullarray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int arr[]= new int[10];
+        for(int i=0;i<10;i++){
+        System.out.println(arr[i]);
+        }
+        }
+    }
+
