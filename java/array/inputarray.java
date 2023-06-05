c1c5f274e549842c5d91f3b709923032d7efd10d
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-06-05 22:07:26 +0530

    Add files via upload

---
+++ java/array/inputarray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Oarray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[]= new int[size];
+        for(int i=0;i<size;i++){
+        arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+        System.out.println(arr[i]);
+        }
+        }
+    }
