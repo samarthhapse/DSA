be56ada64df16cd095d4f57231cd100394325956
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-06-08 23:57:26 +0530

    Add files via upload

---
+++ java/array/inputTwoDimensional.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class inputTwoDimensional{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+    System.out.println("enter the size of matrix");
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int arr[][]=new int[m][n];
+        System.out.println("enter elements of 2D array");
+        for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+            arr[i][j]=sc.nextInt();
+           }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+             System.out.print(arr[i][j]);
+            }
+            System.out.println();
+         }
+    }
+}
