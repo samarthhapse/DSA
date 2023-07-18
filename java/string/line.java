cd8ec30971db29da4935f76c08fe292e060ec266
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-07-18 23:31:06 +0530

    Create line.java

---
+++ java/string/line.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class line {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // to input just one word we use sc.nextInt();
+        String fullname=sc.nextLine();
+        System.out.println(fullname); 
+    }
+}
