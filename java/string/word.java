e06d8c668f849329b1e2b91f879ffec7f4cc3aba
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-07-24 23:22:46 +0530

    Add files via upload

---
+++ java/string/word.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class word {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // to input just one word we use sc.nextInt();
+        String name=sc.next();
+        System.out.println(name); 
+        
+    }
+}
