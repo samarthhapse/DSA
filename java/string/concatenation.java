1ac52080bdf46b8ebd070db6d6e99a55dd4e2d88
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-07-24 23:23:45 +0530

    Add files via upload

---
+++ java/string/concatenation.java
@@ -0,0 +1,9 @@
+public class concatenation {
+    public static void main(String args[]){
+        // concatention
+        String first="sama";
+        String last="hapse";
+        String full=first+ " " +last;
+        System.out.println(full);
+    }
+}
