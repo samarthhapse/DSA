cb019c35b32e5de2599fde48780959e703db5e3f
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-07-24 23:23:13 +0530

    Add files via upload

---
+++ java/string/substring.java
@@ -0,0 +1,9 @@
+public class substring {
+    public static void main(String srgs[]){
+String name="samarth";
+// substring(beg index, end index);
+System.out.println(name.substring(0, 5));
+// if we do not pass end index it consider till end of string 
+System.out.println(name.substring(3));
+    }    
+}
