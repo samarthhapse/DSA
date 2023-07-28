d63d8e40b49dbd7c7eb31a003b7269f9daa01aac
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2023-07-28 23:22:38 +0530

    Add files via upload

---
+++ java/string/charAt.java
@@ -0,0 +1,13 @@
+public class charAt {
+    public static void main(String args[]){
+        // charAt();
+        String first="sama";
+        String last="hapse";
+        String full=first+ "@" +last;
+        for(int i=0;i<first.length();i++){
+        System.out.println(first.charAt(i)); 
+         //to calculate no. of charecters in any string
+        System.out.println(full.length()); 
+    }
+}  
+}
